f0698110e76718da796d26b227f6151bee351398
emsbe <emma-sophie.betz@web.de>
2025-09-15 08:47:38 +0000

    Add status

--- src/main/java/com/example/cms/dto/PostResponse.java
+++ src/main/java/com/example/cms/dto/PostResponse.java
@@ -1,6 +1,8 @@
 package com.example.cms.dto;
 
+import com.example.cms.model.Status;
+
 import java.util.List;
 import java.time.LocalDateTime;
 
-public record PostResponse(Long id, String title, String author, String content, LocalDateTime createdAt, List<String> categories) {}
+public record PostResponse(Long id, String title, String author, String content, LocalDateTime createdAt, Status status, List<String> categories) {}

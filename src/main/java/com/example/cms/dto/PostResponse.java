b3177db79ae30b98dea4e86cd9f18bb86c50222b
emsbe <emma-sophie.betz@web.de>
2025-09-15 17:27:26 +0000

    Adjust DTO to return blogpostId and versionNumber

--- src/main/java/com/example/cms/dto/PostResponse.java
+++ src/main/java/com/example/cms/dto/PostResponse.java
@@ -5,4 +5,4 @@ import com.example.cms.model.Status;
 import java.util.List;
 import java.time.LocalDateTime;
 
-public record PostResponse(Long id, String title, String author, String content, LocalDateTime createdAt, Status status, List<String> categories) {}
+public record PostResponse(Long blogpostId, Integer versionNumber, String title, String author, String content, LocalDateTime createdAt, Status status, List<String> categories) {}
